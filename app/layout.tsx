2b99dac866cb438f848bec30962397e62661996c
Bine Cadez <bine.cadez@gmail.com>
2025-08-22 11:19:38 +0200

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,16 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Praznovanje',
     description: 'Dobrodošli na moji 50tki!',
-    images: [{ url: '/og.jpg', width: 1200, height: 630 }],
+    type: 'website',
+    images: [
+      { url: 'https://bb50.si/opengraph-image.jpg', width: 1200, height: 630 },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
-    images: ['/og.jpg'],
+    images: [
+      { url: 'https://bb50.si/opengraph-image.jpg', width: 1200, height: 630 },
+    ],
   },
 }
 
